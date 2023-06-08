1d3aa68473e9a95271c32f12e6a1be625a1c34f1
AP-THAI\wannasak_p <wannasak.pen@gmail.com>
2023-06-08 09:47:37 +0700

    add HealthCheck

---
+++ AP_MediaService.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AP_MediaService.API.Controllers
+{
+    public class HealthCheckController : Controller
+    {
+        [Route("[controller]")]
+        [HttpGet]
+        public ContentResult HealthCheck()
+        {
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                Content = "<div>HealthCheck</div>"
+            };
+        }
+    }
+}
